958fd5fff5a942faaddc088924193aa738f458af
kvipka90 <kvipka90@yandex.ru>
2023-01-06 21:32:19 +0300

    implement cache

--- src/main/java/com/atwow/AuthBackend/repos/AccountRepository.java
+++ src/main/java/com/atwow/AuthBackend/repos/AccountRepository.java
@@ -1,11 +1,13 @@
 package com.atwow.AuthBackend.repos;
 
 import com.atwow.AuthBackend.units.Account;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends CrudRepository<Account, Long> {
+    @Cacheable("account")
     Account findByUsername(String username);
 }
 
